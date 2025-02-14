038543ed4cedef2ad1ac97bf09dd783715da3c32
demanl0310 <ddema0310@gmail.com>
2025-02-14 10:14:43 +0800

    renderer zasav

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape tegshUn = new Rectangle(2, 3);
+        Shape kvad = new Square(5);
+        Shape dugui = new Circle(8);
 
-        Renderer renderer = new Renderer(rectangle);
-
-        renderer.draw();
+        Renderer renderer1 = new Renderer(dugui);
+        renderer1.draw();
+        Renderer renderer2 = new Renderer(tegshUn);
+        renderer2.draw();
+        Renderer renderer3 = new Renderer(kvad);
+        renderer3.draw();
     }
 }
